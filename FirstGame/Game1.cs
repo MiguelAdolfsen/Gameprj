ff68c577bba0a2f38d3b50c91c9db18daa1f234c
MiguelAdolfsen <miguel.adolfsen@gmail.com>
2025-03-30 01:55:26 +0100

    awd

--- FirstGame/Game1.cs
+++ FirstGame/Game1.cs
@@ -14,6 +14,7 @@ public class Game1 : Game
         _graphics = new GraphicsDeviceManager(this);
         Content.RootDirectory = "Content";
         IsMouseVisible = true;
+        Window.Title = "First Game";
     }
 
     protected override void Initialize()
